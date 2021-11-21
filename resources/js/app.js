c823bd4bfdc4ad14db65a8010e37c13191054851
diegodisant <developerdiego0@gmail.com>
2021-11-21 04:52:23 -0600

    config: add support for vue js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 require('./bootstrap');
+
+// Deps
+import Vue from 'vue';
+
+// Components
+import AppComponent from '../components/app';
+
+Vue.component('app', AppComponent)
+
+const app = new Vue({
+    el: '#app',
+    components: {}
+});
