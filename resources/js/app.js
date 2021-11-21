55edbfdce7e14ee9cb76cd1c45c4a11b61b8e9ab
diegodisant <developerdiego0@gmail.com>
2021-11-21 08:11:54 -0600

    deps: disable vue support need to organize better

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,8 @@
 require('./bootstrap');
 
 import Alpine from 'alpinejs';
-import Vue from 'vue';
-import AppComponent from '../components/app';
+//import Vue from 'vue';
+//import AppComponent from '../components/app';
 
 // Alpine bootstrap
 window.Alpine = Alpine;
@@ -10,9 +10,12 @@ window.Alpine = Alpine;
 Alpine.start();
 
 // Vue Components
-Vue.component('app', AppComponent);
+/**
+ * @ TODO add each application per module
+ */
+//Vue.component('app', AppComponent);
 
-const app = new Vue({
+/*const app = new Vue({
     el: '#app',
     components: {},
-});
+});*/
