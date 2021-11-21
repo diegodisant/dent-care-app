74ab365757924cf3ab12c7638b5e3d5df3bcdea0
diegodisant <developerdiego0@gmail.com>
2021-11-21 07:58:49 -0600

    deps: restore vue support

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,18 @@
 require('./bootstrap');
 
 import Alpine from 'alpinejs';
+import Vue from 'vue';
+import AppComponent from '../components/app';
 
+// Alpine bootstrap
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// Vue Components
+Vue.component('app', AppComponent);
+
+const app = new Vue({
+    el: '#app',
+    components: {},
+});
