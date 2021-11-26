9800ea671c64f44293eee495f900a4a356d12f8e
diegodisant <developerdiego0@gmail.com>
2021-11-26 16:00:15 -0600

    refactor: disable default login view

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/vue', function () {
-   return view('home');
+    return view('auth/login');
 });
 
 Route::get('/dashboard', function () {
