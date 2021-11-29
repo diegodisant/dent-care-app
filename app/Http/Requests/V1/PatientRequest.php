d9fd4aa6d9482012f48d6367c033e18bbb31b798
diegodisant <developerdiego0@gmail.com>
2021-11-29 10:15:04 -0600

    chore: add api requests

---
+++ app/Http/Requests/V1/PatientRequest.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Requests\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PatientRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+}
