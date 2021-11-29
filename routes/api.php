07d023353ca7ab26fa1fc64e4c5ca52b3dabfe47
diegodisant <developerdiego0@gmail.com>
2021-11-28 23:03:42 -0600

    config(api): mount all API controllers

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\UserController;
+use App\Http\Controllers\Api\V1\PatientController;
+use App\Http\Controllers\Api\V1\AppointmentController;
+use App\Http\Controllers\Api\V1\MedicalNoteController;
+use App\Http\Controllers\Api\V1\ClinicalHistoryController;
+use App\Http\Controllers\Api\V1\ClinicalHistoryAnswerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +23,40 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource(
+    '/v1/users',
+    UserController::class
+)
+    ->middleware('api');
+
+Route::apiResource(
+    '/v1/patients',
+    PatientController::class
+)
+    ->middleware('api');
+
+Route::apiResource(
+    '/v1/appointments',
+    AppointmentController::class
+)
+    ->middleware('api');
+
+Route::apiResource(
+    '/v1/medical_notes',
+    MedicalNoteController::class
+)
+    ->middleware('api');
+
+Route::apiResource(
+    '/v1/clinical_histories',
+    ClinicalHistoryController::class
+)
+    ->middleware('api');
+
+Route::apiResource(
+    '/v1/clinical_history_answers',
+    ClinicalHistoryAnswerController::class
+)
+    ->middleware('api');
+
